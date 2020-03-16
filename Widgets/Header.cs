f53c367ac1329c750fbe4f2ca312fe3d36a0461e
Andrey Maksimov <meamka@ya.ru>
2020-03-16 23:41:44 +0300

    Add base menu popover

--- Widgets/Header.cs
+++ Widgets/Header.cs
@@ -6,15 +6,16 @@ namespace Muon.Widgets
     {
         public Button OpenButton;
         public Button SaveButton;
-
+        public Button MenuButton => menuButton;
+        MenuButton menuButton;
 
         public Header()
         {
             Title = "Muon";
-            Subtitle = "the editor";
+            Subtitle = "";
             HasSubtitle = Subtitle.Length > 0;
             ShowCloseButton = true;
-            StyleContext.AddClass("default-decoration");
+            // StyleContext.AddClass("default-decoration");
 
             OpenButton = new Button("document-open", IconSize.LargeToolbar);
             OpenButton.TooltipMarkup = "Open document\n<span weight='600' size='smaller' alpha='75%'>Ctrl+O</span>";
@@ -24,8 +25,56 @@ namespace Muon.Widgets
             PackStart(OpenButton);
             PackStart(SaveButton);
 
-            var menuButton = new MenuButton();
+            // Init menu action buttons
+            var zoomOutButton = new Button("zoom-out-symbolic", IconSize.Menu)
+            {
+                ActionName = "win.zoom_out",
+            };
+
+            var zoomDefaultButton = new Button("100%")
+            {
+                ActionName = "win.zoom_reset",
+            };
+
+            var zoomInButton = new Button("zoom-in-symbolic", IconSize.Menu)
+            {
+                ActionName = "win.zoom_in",
+            };
+
+            var fontSizeGrid = new Grid()
+            {
+                ColumnHomogeneous = true,
+                Hexpand = true,
+                Margin = 12,
+            };
+            fontSizeGrid.StyleContext.AddClass("linked");
+            fontSizeGrid.Add(zoomOutButton);
+            fontSizeGrid.Add(zoomDefaultButton);
+            fontSizeGrid.Add(zoomInButton);
+
+            var menuSeparator = new Separator(Orientation.Horizontal);
+            menuSeparator.MarginTop = 12;
+
+            var menuGrid = new Grid()
+            {
+                MarginBottom = 3,
+                Orientation = Orientation.Vertical,
+                WidthRequest = 200,
+            };
+
+            menuGrid.Attach(fontSizeGrid, 0, 0, 3, 1);
+            menuGrid.Attach(menuSeparator, 0, 2, 3, 1);
+            menuGrid.ShowAll();
+
+            menuButton = new MenuButton();
+            menuButton.Image = new Image("open-menu-symbolic", IconSize.LargeToolbar);
+            menuButton.TooltipText = "Menu";
+            menuButton.ShowAll();
             PackEnd(menuButton);
+
+            var popover = new Popover(MenuButton);
+            popover.Add(menuGrid);
+            menuButton.Popover = popover;
         }
     }
 }
