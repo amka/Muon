1b582f3fc2e4c4be2442de480fc6287abb3b1971
Andrey Maksimov <meamka@ya.ru>
2020-03-28 21:13:20 +0300

    Move popover to class

--- Widgets/Header.cs
+++ Widgets/Header.cs
@@ -19,61 +19,20 @@ namespace Muon.Widgets
 
             OpenButton = new Button("document-open", IconSize.LargeToolbar);
             OpenButton.TooltipMarkup = "Open document";
-            SaveButton = new Button("document-save", IconSize.LargeToolbar);
+            SaveButton = new Button("document-save-as", IconSize.LargeToolbar);
             SaveButton.TooltipMarkup = "Save document as...";
 
             PackStart(OpenButton);
-            PackEnd(SaveButton);
-
-            // Init menu action buttons
-            var zoomOutButton = new Button("zoom-out-symbolic", IconSize.Menu)
-            {
-                ActionName = "win.zoom_out",
-            };
-
-            var zoomDefaultButton = new Button("100%")
-            {
-                ActionName = "win.zoom_reset",
-            };
-
-            var zoomInButton = new Button("zoom-in-symbolic", IconSize.Menu)
-            {
-                ActionName = "win.zoom_in",
-            };
-
-            var fontSizeGrid = new Grid()
-            {
-                ColumnHomogeneous = true,
-                Hexpand = true,
-                Margin = 12,
-            };
-            fontSizeGrid.StyleContext.AddClass("linked");
-            fontSizeGrid.Add(zoomOutButton);
-            fontSizeGrid.Add(zoomDefaultButton);
-            fontSizeGrid.Add(zoomInButton);
-
-            var menuSeparator = new Separator(Orientation.Horizontal);
-            menuSeparator.MarginTop = 12;
-
-            var menuGrid = new Grid()
-            {
-                MarginBottom = 3,
-                Orientation = Orientation.Vertical,
-                WidthRequest = 200,
-            };
-
-            menuGrid.Attach(fontSizeGrid, 0, 0, 3, 1);
-            menuGrid.Attach(menuSeparator, 0, 2, 3, 1);
-            menuGrid.ShowAll();
+            PackStart(SaveButton);
 
             menuButton = new MenuButton();
-            menuButton.Image = new Image("open-menu-symbolic", IconSize.LargeToolbar);
+            menuButton.Image = Image.NewFromIconName("open-menu", IconSize.LargeToolbar);
             menuButton.TooltipText = "Menu";
             menuButton.ShowAll();
             PackEnd(menuButton);
 
-            var popover = new Popover(MenuButton);
-            popover.Add(menuGrid);
+            // Connect Popover
+            var popover = new MenuPopover(menuButton);
             menuButton.Popover = popover;
         }
     }
---
+++ Widgets/MenuPopover.cs
@@ -0,0 +1,62 @@
+using System;
+using Gtk;
+
+namespace Muon.Widgets
+{
+    public class MenuPopover : Popover
+    {
+        public event EventHandler PreferenceClicked;
+        public event EventHandler ZoomOutClicked;
+        public event EventHandler ZoomDefaultClicked;
+        public event EventHandler ZoomInClicked;
+
+        public MenuPopover(Widget relativeTo) : base(relativeTo)
+        {
+            // Init menu action buttons
+            var zoomOutButton = new Button("zoom-out-symbolic", IconSize.Menu);
+            zoomOutButton.ActionName = "win.zoom_out";
+
+            var zoomDefaultButton = new Button("100%");
+            zoomDefaultButton.ActionName = "win.zoom_reset";
+
+            var zoomInButton = new Button("zoom-in-symbolic", IconSize.Menu);
+            zoomInButton.ActionName = "win.zoom_in";
+
+            var fontSizeGrid = new Grid()
+            {
+                ColumnHomogeneous = true,
+                Hexpand = true,
+                Margin = 12,
+            };
+            fontSizeGrid.StyleContext.AddClass("linked");
+            fontSizeGrid.Add(zoomOutButton);
+            fontSizeGrid.Add(zoomDefaultButton);
+            fontSizeGrid.Add(zoomInButton);
+
+            var menuSeparator = new Separator(Orientation.Horizontal);
+            menuSeparator.MarginTop = 12;
+
+            var prefButton = new ModelButton();
+            prefButton.Text = "Preferences";
+            // prefButton.ActionName = "win.open_preferences";
+            prefButton.Clicked += (sender, args) => {
+                Console.WriteLine("Pref clicked");
+            };
+
+            var menuGrid = new Grid()
+            {
+                MarginBottom = 3,
+                Orientation = Orientation.Vertical,
+                WidthRequest = 200,
+            };
+
+            menuGrid.Attach(fontSizeGrid, 0, 0, 3, 1);
+            menuGrid.Attach(menuSeparator, 0, 2, 3, 1);
+            menuGrid.Attach(prefButton, 0, 3, 3, 1);
+            
+            menuGrid.ShowAll();
+
+            Add(menuGrid);
+        }
+    }
+}
